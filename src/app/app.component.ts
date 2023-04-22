cd29a80282176a8a978f45d526e47c99ac6580c3
Carlos <arlo252@gmail.com>
2023-04-22 16:39:42 -0400

    icons are plenty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'To Do List', url: 'to-do-list', icon: 'mail' },
-    { title: 'Goals', url: 'goals', icon: 'paper-plane' },
-    { title: 'Achievements', url: 'achievements', icon: 'heart' },
-    { title: 'Notes', url: 'notes', icon: 'archive' },
-    { title: 'Video Diary', url: 'video-diary', icon: 'trash' },
-    { title: 'Images Diary', url: 'images-diary', icon: 'warning' },
+    { title: 'To Do List', url: 'to-do-list', icon: 'clipboard' },
+    { title: 'Goals', url: 'goals', icon: 'football' },
+    { title: 'Achievements', url: 'achievements', icon: 'trophy' },
+    { title: 'Notes', url: 'notes', icon: 'newspaper' },
+    { title: 'Video Diary', url: 'video-diary', icon: 'videocam' },
+    { title: 'Images Diary', url: 'images-diary', icon: 'images' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Family', 'Friends', 'Code', 'Hobbies', 'Adele', 'Wedding'];
   constructor() {}
 }
